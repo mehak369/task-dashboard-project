d25c735cec4adf45831171cc0a521fe7c96d8428
mehak369 <mehakg0369@gmail.com>
2025-12-19 20:00:42 +0530

    Update auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -32,8 +32,8 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
